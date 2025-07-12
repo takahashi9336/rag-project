2d0e3eecb9eef94c6295fbc1f404e70e9d06b9d5
takahashi9336 <s.takahashi9336@gmail.com>
2025-07-12 20:18:36 +0900

    構成変更

--- app.py
+++ app.py
@@ -1,33 +1,6 @@
 # main/app.py
-import parser.c_parser as c_parser
-import vectorestore.chroma_connector as chroma_connector
 import query
-
-print("==" * 10 + " starting get_chunks" + "==" * 10)
-
-chunks = c_parser.get_chunks('./batch_source/sample_test.c')
-# chunks = c_source_loader.get_chunks_from_directory('./source')
-
-print("==" * 10 + " starting to add chunks to vector database " + "==" * 10)
-
-db = chroma_connector.get_vector_db()
-for chunk in chunks:
-    db.add_texts(
-        texts=[chunk['code']],
-        metadatas=[{
-            'name': chunk['name'],
-            'start_line': chunk['start_line'],
-            'end_line': chunk['end_line'],
-            'bytes': chunk['bytes']
-        }]
-    )
-
-print(f"Added {len(chunks)} function definitions to the vector database.")
-
-print("==" * 10 + " starting query" + "==" * 10)
-
 query_input = "sample_test.cの処理概要を説明して。"
 response = query.query(query_input)
 print(f"Response to query '{query_input}': {response}")
-
 
---
+++ vectorestore/init_vectorstore.py
@@ -0,0 +1,19 @@
+import parser.c_parser as c_parser
+import vectorestore.chroma_connector as chroma_connector
+
+chunks = c_parser.get_chunks('./batch_source/sample_test.c')
+# chunks = c_source_loader.get_chunks_from_directory('./source')
+
+db = chroma_connector.get_vector_db()
+for chunk in chunks:
+    db.add_texts(
+        texts=[chunk['code']],
+        metadatas=[{
+            'name': chunk['name'],
+            'start_line': chunk['start_line'],
+            'end_line': chunk['end_line'],
+            'bytes': chunk['bytes']
+        }]
+    )
+
+print(f"Added {len(chunks)} function definitions to the vector database.")
